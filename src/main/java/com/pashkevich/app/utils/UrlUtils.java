ace5830347dcd90c37927647a40af8279b7f7570
l0nsdale <vlad-96-cool@mail.ru>
2017-04-10 22:18:08 +0300

    add urlutils

---
+++ src/main/java/com/pashkevich/app/utils/UrlUtils.java
@@ -0,0 +1,30 @@
+package com.pashkevich.app.utils;
+
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.pashkevich.app.constants.Constants.Common.EMPTY;
+
+/**
+ * Created by Vlad on 25.03.17.
+ */
+public class UrlUtils {
+
+    public static String getAppUrl(WebRequest request) {
+        HttpServletRequest httpServletRequest = ((ServletWebRequest) request).getRequest();
+        String url = EMPTY;
+        url += httpServletRequest.getScheme();
+        url += "://";
+        url += httpServletRequest.getServerName();
+        url += ":";
+        url += httpServletRequest.getServerPort();
+        if (!httpServletRequest.getContextPath().isEmpty()) {
+            url += "/";
+            url += httpServletRequest.getContextPath();
+        }
+        return url;
+    }
+
+}
